583574261641e8c1689947fb83bedd967612bdc1
Aaron Ostroski <44450422+aaronostroski@users.noreply.github.com>
2022-05-16 20:07:26 -0300

    clean js file

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,24 +2,6 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-// const targetEl = document.getElementById('defaultModal');
-
-// const options = {
-//   placement: 'bottom-right',
-//   backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
-//   onHide: () => {
-//       console.log('modal is hidden');
-//   },
-//   onShow: () => {
-//       console.log('modal is shown');
-//   },
-//   onToggle: () => {
-//       console.log('modal has been toggled');
-//   }
-// };
-
-// const modal = new Modal(targetEl, options);
-
 const toggleLikeButton = (elem, id) => {
     elem.classList.toggle("hidden");
     const unlikeButton = document.querySelector(`[data-id='${id}'].unlike`);
@@ -43,16 +25,16 @@ const likeButton = document.querySelectorAll(".like");
 likeButton.forEach(elem => {
     elem.addEventListener("click", ev => {
         ev.preventDefault();
-        const idPost = elem.dataset.id;
+        const { id } = elem.dataset;
         const data = new FormData();
-        data.append("post_id", idPost );
+        data.append("post_id", id );
 
         fetch("/posts_likes" , {"method": "POST", body: data})
         .then( async res => {
             if(res.ok) {
                 const { response: total } = await res.json();
-                changeCounter(idPost, total);
-                toggleLikeButton(elem, idPost)
+                changeCounter(id, total);
+                toggleLikeButton(elem, id)
             }
         })
         .catch(e => console.error(e));
@@ -65,16 +47,16 @@ const unlikeButton = document.querySelectorAll(".unlike");
 unlikeButton.forEach(elem => {
     elem.addEventListener("click", ev => {
         ev.preventDefault();
-        const idPost = elem.dataset.id;
+        const { id } = elem.dataset;
         const data = new FormData();
-        data.append("post_id", idPost );
+        data.append("post_id", id );
 
-        fetch(`/posts_likes/${idPost}` , {"method": "DELETE"})
+        fetch(`/posts_likes/${id}` , {"method": "DELETE"})
         .then( async res => {
             if(res.ok) {
                 const { response: total } = await res.json();
-                changeCounter(idPost, total);
-                toggleUnlikeButton(elem, idPost)
+                changeCounter(id, total);
+                toggleUnlikeButton(elem, id)
             }
         })
         .catch(e => console.error(e));
