faf421563bb8bc1e84163ac5657a9e4ac05c45ea
Aaron Ostroski <44450422+aaronostroski@users.noreply.github.com>
2022-05-16 19:22:34 -0300

    config: change to Sao Paulo timezone

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module LameBook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.time_zone = 'America/Sao_Paulo'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
