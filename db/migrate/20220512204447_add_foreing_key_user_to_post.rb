698886ce36a61f6c48a818151ed7445135efec6f
Aaron Ostroski <44450422+aaronostroski@users.noreply.github.com>
2022-05-16 22:43:45 -0300

    fix: db migrate

--- db/migrate/20220512204447_add_foreing_key_user_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddForeingKeyUserToPost < ActiveRecord::Migration[7.0]
-  def change
-        add_foreign_key :posts, :users
-  end
-end
--- db/migrate/20220512224526_add_foreing_key_user_to_posts_likes.rb
+++ db/migrate/20220512224526_add_foreing_key_user_to_posts_likes.rb
@@ -1,5 +1,5 @@
 class AddForeingKeyUserToPostsLikes < ActiveRecord::Migration[7.0]
   def change
-      add_foreign_key :posts_likes, :users
+    add_foreign_key :posts_likes, :users
   end
 end
