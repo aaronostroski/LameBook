42a97dc2029f79b274e65bcd62fe7f173e75debd
Aaron Ostroski <44450422+aaronostroski@users.noreply.github.com>
2022-05-16 21:40:21 -0300

    improve: add validation fields in user, post and post_likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_one_attached :image
+    validates :text, presence: true
+    validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  validates :firstName, presence: true
+  validates :lastName, presence: true
 end
